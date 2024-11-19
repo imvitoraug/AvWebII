cc536350c1d8766a4ea1f5f8c64531a47c34157d
Vitor Augusto <144061328+imvitoraug@users.noreply.github.com>
2024-11-19 20:07:01 -0300

    Add files via upload

---
+++ AvWebII/cesar.js
@@ -0,0 +1,18 @@
+let alfabeto = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+let num = alfabeto
+
+function criptografia(mensagem, chave, converter){
+    if(mensagem.lenght > 0){
+        for(let i = 0; tam = mensagem.length; i < tam; i++){
+
+        }
+    }
+}
+
+function cifraLetra(letra, chave){
+
+}
+
+function decifraLetra(letra, chave){
+    
+}
